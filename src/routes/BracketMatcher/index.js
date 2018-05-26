afd30180cb21aa9bf7dabac7ede1390cf64c4711
Syam Pillai <syam.sp0965@gmail.com>
2018-05-26 18:46:06 +0530

    small fix

--- src/routes/BracketMatcher/index.js
+++ src/routes/BracketMatcher/index.js
@@ -168,7 +168,7 @@ class BracketMatcher extends Component {
    */
   getErrorRowAndColumn = (codeSnippet, position) => {
     const rowColumnArray = JSON.stringify(codeSnippet.slice(0, position)).split(/\\n/) || []
-    const lineNumber = rowColumnArray.length;
+    const lineNumber = rowColumnArray.length - 1;
     const columnNumber = rowColumnArray[lineNumber - 1].length
     return {
       lineNumber: lineNumber,
